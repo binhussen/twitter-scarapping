d0eb487ae4d93809dae2c1217907df9dd85295f7
Mohammed <mhdhsna@gmail.com>
2024-05-07 12:50:33 +0300

    Add Service

---
+++ src/services/twitService.js
@@ -0,0 +1,17 @@
+import Twit from '../models/twit.js';
+class TwitService {
+    static async getTwits(page, pageSize) {
+        const limit = pageSize ? +pageSize : undefined;
+        let offset = page ? (+page - 1) * limit : undefined;
+
+        try {
+            return await Twit.findAndCountAll({
+                limit: limit,
+                offset: offset
+            });
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+export default TwitService;
