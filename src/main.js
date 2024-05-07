960315a6a26fa6bf1e9751b1e43af456abd83100
Mohammed <mhdhsna@gmail.com>
2024-05-07 12:52:21 +0300

    Add Start up Server

---
+++ src/main.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import database from './config/database.js';
+import TwitController from './controllers/twitController.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000
+
+database.authenticate()
+    .then(() => console.log('Database connected....'))
+    .catch(error => console.log('Database Error: ' + error));
+
+// Body parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.get('/twits',TwitController.getTwits);
+
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
